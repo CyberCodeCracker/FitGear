ba10841b8ffdf54f56d2359793591a1f259145a8
CyberCodeCracker <souhailamouri123@gmail.com>
2025-02-19 21:18:23 +0100

    Implemented authenticationProvider bean to verify if user has right credentials

---
+++ FitGear/fitgear-api/src/main/java/com/amouri_coding/FitGear/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.amouri_coding.FitGear.security;
+
+import com.amouri_coding.FitGear.user.User;
+import com.amouri_coding.FitGear.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        System.out.println("Loading user by email: " + userEmail);
+        User user = repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException(userEmail + " not found."));
+        return user;
+    }
+}
---
+++ FitGear/fitgear-api/src/main/java/com/amouri_coding/FitGear/user/User.java
@@ -0,0 +1,58 @@
+package com.amouri_coding.FitGear.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.security.Principal;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class User implements UserDetails, Principal {
+
+    @Id
+    private Integer id;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+}
